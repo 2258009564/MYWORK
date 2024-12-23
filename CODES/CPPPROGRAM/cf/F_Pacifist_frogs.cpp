6fea84edfddbe80b8b01cd01855ae89e78581089
alisa <2258009564@qq.com>
2024-12-23 18:59:18 +0800

    重构 F. Pacifist frogs 问题代码，删除冗余文件并添加问题配置文件

--- CODES/CPPPROGRAM/cf/F_Pacifist_frogs.cpp
+++ CODES/CPPPROGRAM/cf/F_Pacifist_frogs.cpp
@@ -18,9 +18,6 @@ typedef vector<vd> vvd;
 typedef vector<char> vc;
 typedef vector<vc> vvc;
 typedef vector<vvc> vvvc;
-typedef vector<bool> vb;
-typedef vector<vb> vvb;
-typedef vector<vvb> vvvb;
 typedef map<int, int> mii;
 typedef map<char, int> mci;
 #define endl '\n'
@@ -50,35 +47,30 @@ ostream &operator<<(ostream &out, const vector<T> &v)
 
 void solve()
 {
+    // 读入小岛数量n, 选择的青蛙数量m, 蚊子数量k
     int n, m, k;
     cin >> n >> m >> k;
+
+    // 读入m个青蛙的数值
     vi frogs(m);
     cin >> frogs;
+
+    // 用map记录每个蚊子出现的位置
     mii mosks;
     int num;
-    for (int i = 0; i < k; i++)
+    for (int i = 1; i <= k; i++)
     {
         cin >> num;
         mosks[num]++;
     }
 
-    map<int, vector<int>> mp;
-
-    for (int i = 0; i < m; i++)
-
+    // mp用于统计每个青蛙能吃到的蚊子数量，key是蚊子数量，value是青蛙的编号
+    map<int, vi> mp;
+    for (int i = 0; i < frogs.size(); i++)
     {
         auto frog = frogs[i];
         int total = 0;
-        // int index = 1;
-        // while (index <= n)
-        // {
-        //     if (mosks[frog * index])
-        //     {
-        //         total++;
-        //     }
-        //     index++;
-        // }
-
+        // 遍历每个蚊子，如果蚊子的数值能被青蛙整除，说明这个青蛙能吃到这个蚊子
         for (auto &&[mosk, v] : mosks)
         {
             if (mosk % frog == 0)
@@ -87,13 +79,14 @@ void solve()
             }
         }
 
+        // 记录能吃到相同数量蚊子的青蛙编号
         mp[total].push_back(i + 1);
     }
-    
-    auto vec = (*mp.begin()).second;
-    cout << vec.size() << endl;
-    sort(all(vec));
-    cout << vec;
+
+    // 输出能吃到最少蚊子的青蛙数量和编号
+    // mp是按key升序的，所以第一个元素就是最少的
+    cout << (*mp.begin()).second.size() << endl;
+    cout << (*mp.begin()).second;
 }
 
 signed main()

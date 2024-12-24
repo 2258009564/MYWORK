15e07568f36ba96f5e06923551ca86c7569a417c
alisa <2258009564@qq.com>
2024-12-24 15:53:07 +0800

    删除无用的 C++ 文件并添加学生类的实现

--- CODES/CPPPROGRAM/c++/things about class.cpp
+++ CODES/CPPPROGRAM/c++/things about class.cpp
@@ -45,38 +45,78 @@ ostream &operator<<(ostream &out, const vector<T> &v)
     return out;
 }
 
-vvi result;
-vi path;
-
-void backtracking(vi &v, int startindex)
+// 定义学生类
+class stu
 {
-    if (path.size() > 1)
-    {
-        cout << path;
-        cout << endl;
-    }
-    if (startindex == v.size())
-    {
-        return;
-    }
+private:
+    int age;        // 年龄
+    string name;    // 姓名
 
-    for (int i = startindex; i < v.size(); i++)
+public:
+    // 构造函数，使用初始化列表初始化成员变量
+    stu(int a, string n) : age(a), name(n) {}
+    // 默认构造函数
+    stu() {}
+    
+    // 设置学生信息的成员函数
+    void set(int a, string n)
     {
-        path.push_back(v[i]);
-        backtracking(v, i + 1);
-        path.pop_back();
+        age = a;
+        name = n;
     }
-}
+    
+    // 打印学生信息
+    void print()
+    {
+        cout << age << ' ' << name << endl;
+    }
+    
+    // 设置年龄
+    void setage(int a)
+    {
+        age = a;
+    }
+    
+    // 设置姓名
+    void setname(string n)
+    {
+        name = n;
+    }
+    
+    // 获取年龄
+    int getage()
+    {
+        return age;
+    }
+    
+    // 获取姓名
+    string getname()
+    {
+        return name;
+    }
+    
+    // 析构函数
+    ~stu() {}
+};
 
+// 主要的解决函数
 void solve()
 {
-    vi v = {1, 2, 3, 4, 5, 6};
-    backtracking(v, 0);
-    for (auto &&i : result)
-    {
-        cout << i;
-        cout << endl;
-    }
+    // 创建学生对象并初始化
+    stu a(18, "zhangsan");
+    a.print();  // 输出：18 zhangsan
+    
+    // 修改学生信息
+    a.set(20, "lisi");
+    a.print();  // 输出：20 lisi
+    
+    // 分别设置年龄和姓名
+    a.setage(22);
+    a.setname("wangwu");
+    a.print();  // 输出：22 wangwu
+    
+    // 获取并输出学生信息
+    cout << a.getage() << ' ' << a.getname() << endl;  // 输出：22 wangwu
 }
 
 signed main()

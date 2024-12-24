8f7d6d6a65e91699ead07c080ca5612266075c45
alisa <2258009564@qq.com>
2024-12-24 17:29:14 +0800

    添加多个 C++ 题目实现，包括圆类计算圆环面积、类继承、包含静态成员的人类定义、以及 Boat 和 Car 类的友元函数

---
+++ CODES/CPPPROGRAM/NEWCODER/封装_定义人类_包含静态成员.cpp
@@ -0,0 +1,89 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef vector<vvs> vvvs;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<pdd> vpdd;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+
+// 通用版本的 >> 重载，用于任意类型的 std::vector<T>
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (auto &x : v)
+    {
+        in >> x;
+    }
+    return in;
+}
+
+// 通用版本的 << 重载，用于任意类型的 std::vector<T>
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+    for (const auto &x : v)
+    {
+        out << x << ' ';
+    }
+    return out;
+}
+
+class People
+{
+private:
+    string name;
+    int age, height, weight;
+
+public:
+    static int count;
+    People(string a, int b, int c, int d) : name(a), age(b), height(c), weight(d) { count++; };
+
+    static void show()
+    {
+        cout << count;
+    }
+};
+int People::count = 0;
+
+void solve()
+{
+    string s;
+    int a, b, c;
+    while (cin >> s)
+    {
+        if (s != "exit")
+        {
+            cin >> a >> b >> c;
+            People person(s, a, b, c);
+        }
+    }
+    People::show();
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int TT = 1;
+    // cin >> TT;
+    while (TT--)
+    {
+        solve();
+    }
+}

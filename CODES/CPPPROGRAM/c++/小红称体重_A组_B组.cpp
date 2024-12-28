f023a8be0a835ab2fe4e0c75b5b8b11f058e79c0
alisa <2258009564@qq.com>
2024-12-28 20:16:19 +0800

    添加小红系列题目的代码实现和配置文件更新

---
+++ CODES/CPPPROGRAM/c++/小红称体重_A组_B组.cpp
@@ -0,0 +1,74 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef vector<vvs> vvvs;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<pdd> vpdd;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+
+// 通用版本的 >> 重载，用于任意类型的 std::vector<T>
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (auto &x : v)
+    {
+        in >> x;
+    }
+    return in;
+}
+
+// 通用版本的 << 重载，用于任意类型的 std::vector<T>
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+    for (const auto &x : v)
+    {
+        out << x << ' ';
+    }
+    return out;
+}
+
+void solve()
+{
+    int n, a;
+    cin >> n >> a;
+    vi v(n);
+    cin >> v;
+    int total = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] >= a and v[i - 1] < a)
+        {
+            total++;
+        }
+    }
+    cout << total;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int TT = 1;
+    // cin >> TT;
+    while (TT--)
+    {
+        solve();
+    }
+}

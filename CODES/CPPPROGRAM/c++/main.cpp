76988cc5fd387d61ecfef047e6d2ef506e25c1fe
alisa <2258009564@qq.com>
2024-12-30 00:00:27 +0800

    重构主程序，添加快速幂取模运算实现，删除无用文件和文档

--- CODES/CPPPROGRAM/c++/main.cpp
+++ CODES/CPPPROGRAM/c++/main.cpp
@@ -1,12 +1,95 @@
-#include "StationeryStore.hpp"
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef vector<vvs> vvvs;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<pdd> vpdd;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define sall(v, x) sort(all(v), x)
 
-int main() {
-    try {
-        UserInterface ui;
-        ui.run();
-    } catch (const std::exception &e) {
-        std::cerr << "程序发生严重错误: " << e.what() << std::endl;
-        return 1;
+// 通用版本的 >> 重载，用于任意类型的 std::vector<T>
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (auto &x : v)
+    {
+        in >> x;
     }
-    return 0;
-} 
+    return in;
+}
+
+// 通用版本的 << 重载，用于任意类型的 std::vector<T>
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+    for (const auto &x : v)
+    {
+        out << x << ' ';
+    }
+    return out;
+}
+
+// 快速幂取模运算
+// @param a: 底数
+// @param b: 指数
+// @param mod: 模数
+// @return: (a^b) % mod
+int QuickPowMod(int a, int b, int mod)
+{
+    if (mod <= 0)
+    {
+        return -1; // 模数必须为正
+    }
+    if (b < 0)
+    {
+        return -1; // 指数必须非负
+    }
+
+    int result = 1;
+    a %= mod;
+    while (b)
+    {
+         if (b & 1)
+        {
+            result = (result * a) % mod;
+        }
+        a = (a * a) % mod;
+        b >>= 1; // 使用位运算替代除法
+    }
+    return result;
+}
+
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int TT = 1;
+    // cin >> TT;
+    while (TT--)
+    {
+        solve();
+    }
+}

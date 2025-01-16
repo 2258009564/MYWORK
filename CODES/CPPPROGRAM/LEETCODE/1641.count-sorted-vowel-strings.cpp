3deee4af505a9a20366f85212520b8b1845cbb67
alisa <2258009564@qq.com>
2025-01-16 22:58:59 +0800

    写了一些水题 更新学习笔记

---
+++ CODES/CPPPROGRAM/LEETCODE/1641.count-sorted-vowel-strings.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+
+/*
+ * @lc app=leetcode.cn id=1641 lang=cpp
+ * @lcpr version=20004
+ *
+ * [1641] 统计字典序元音字符串的数目
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution
+{
+public:
+    int countVowelStrings(int n)
+    {
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 33\n
+// @lcpr case=end
+
+ */

6c9c169d0874b517f938a180ca9e9641c5bb02cf
alisa <2258009564@qq.com>
2025-04-05 14:22:57 +0800

    把没用的东西删掉了~

--- CODES/python practice/PycharmProjects/about excel/使用xlwt创建excel.py
+++
@@ -1,7 +0,0 @@
-import xlwt
-
-#使用xlwt模块创建excel文件
-workbook = xlwt.Workbook()  #创建工作簿
-sheet = workbook.add_sheet('Sheet1')  #创建工作表
-sheet.write(0, 0, 'Hello')  #写入数据
-workbook.save('new excel.xls')  #保存文件

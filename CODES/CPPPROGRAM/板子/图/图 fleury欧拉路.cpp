4542852dae341c256faa1302dbd7728772a2b948
alisa <2258009564@qq.com>
2025-07-04 16:12:05 +0800

    Add comprehensive physics exam materials and personal reflections
    - Created a detailed physics exam document including multiple-choice, short answer, and calculation questions with answers.
    - Added a personal reflection document discussing experiences and insights gained during university life and relationships.
    - Included a new document on digital logic concepts, covering properties of minterms, decoders, and Schmitt triggers.
    - Added a solution document for a specific problem in the CF2116 series.

---
+++ CODES/CPPPROGRAM/板子/图/图 fleury欧拉路.cpp
@@ -0,0 +1,28 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define all(x) begin(x), end(x)
+
+constexpr int MOD = 1e9 + 7;
+// -9.2e18 ~ 9.2e18
+
+void solve()
+{
+    int a = 2;
+    cout << (a++) + (a++) + (a++);
+}
+
+signed main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cout << fixed << setprecision(2);
+    int TT = 1;
+    // cin >> TT;
+    while (TT--)
+    {
+        solve();
+        // cout << endl;
+    }
+}
